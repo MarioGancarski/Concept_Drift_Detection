1f3b0abc3996c40ca8ef17cc464b61d8126e32aa
MarioGancarski <mario.gancarski@protonmail.com>
2022-03-08 14:27:01 +0100

    Re-implementing two algorithms to understand how they work

---
+++ dataset.py
@@ -0,0 +1,21 @@
+import csv
+
+def dataset(dataset_name):
+    X = list()
+    y = list()
+    first = True
+    with open(dataset_name, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quotechar ='|')
+        for row in spamreader:
+          
+            if first :
+                first = False
+            else:
+                elem = list()
+                for i in range (len(row)-1):
+                    elem.append(float(row[i].replace(',', '.')))
+                X.append(elem)
+                y.append(float(row[len(row)-1].replace(',', '.')))
+    return X, y
+
+
